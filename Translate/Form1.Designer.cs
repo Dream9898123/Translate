05b5346b643a33e580dc52514f339d7008758032
Administrator <Administrator@DESKTOP-OR91J3E>
2022-07-26 19:54:11 +0800

    翻译功能

--- Translate/Form1.Designer.cs
+++ Translate/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(530, 417);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Translate/Form1.cs
+++ Translate/Form1.cs
@@ -12,9 +12,45 @@ namespace Translate
 {
     public partial class Form1 : Form
     {
+        private KeyboardHook k_hook;
         public Form1()
         {
             InitializeComponent();
+            k_hook = new KeyboardHook();
+            k_hook.KeyDownEvent += new System.Windows.Forms.KeyEventHandler(hook_KeyDown);//钩住键按下 
+            //k_hook.KeyPressEvent += K_hook_KeyPressEvent;
+            k_hook.Start();//安装键盘钩子
+        }
+
+        private void hook_KeyDown(object sender, KeyEventArgs e)
+        {
+            //判断按下的键（Alt + A）
+            if (e.KeyValue == (int)Keys.T && (int)System.Windows.Forms.Control.ModifierKeys == (int)Keys.Control)
+            {
+                System.Windows.Forms.MessageBox.Show("ddd");
+            }
+        }
+
+        private readonly string keyLoginArray = "ADMIN";
+        private int keyLoginCount = 0;
+        private void K_hook_KeyPressEvent(object sender, KeyPressEventArgs e)
+        {
+            if (true)
+            {
+                if (keyLoginArray[keyLoginCount] == (char)e.KeyChar)
+                {
+                    if (++keyLoginCount == keyLoginArray.Length)
+                    {
+                        MessageBox.Show("提示！");
+                        keyLoginCount = 0;
+                    }
+
+                }
+                else
+                    keyLoginCount = 0;
+
+            }
+            MessageBox.Show("提示");
         }
     }
 }
